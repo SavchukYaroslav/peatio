a33e3b45af4504391e2f5c1fd6e173d9272859af
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-04 16:07:20 +0300

    [ci skip] Start Peatio 1.9.0 development 🎈

--- lib/peatio/version.rb
+++ lib/peatio/version.rb
@@ -2,5 +2,9 @@
 # frozen_string_literal: true
 
 module Peatio
+<<<<<<< HEAD
   VERSION = '1.8.44'
+=======
+  VERSION = '1.9.0.alpha'
+>>>>>>> 8537db2b... [ci skip] Start Peatio 1.9.0 development 🎈
 end
