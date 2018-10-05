0bf8fbdb7a9f33f7166bc5c9cc0f2857797ba872
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-05 18:32:57 +0300

    beautify

--- app/services/ordering.rb
+++ app/services/ordering.rb
@@ -31,7 +31,7 @@ private
 
   def do_submit(order)
     order.fix_number_precision # number must be fixed before computing locked
-    Peatio::FeesService
+    Peatio::FeeService
     order.locked = order.origin_locked = order.compute_locked
     order.save!
     order.hold_account!.lock_funds!(order.locked)
