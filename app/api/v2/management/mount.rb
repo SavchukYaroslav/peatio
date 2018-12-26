f461cafc4ab2b2d1ae76746141dab04ae3d76f3b
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-12-26 18:52:24 +0200

    Use Array of Operations in Transfers API

--- app/api/v2/management/mount.rb
+++ app/api/v2/management/mount.rb
@@ -47,7 +47,7 @@ module API
         mount Management::Withdraws
         mount Management::Tools
         mount Management::Operations
-
+        mount Management::Transfers
       end
     end
   end
--- app/api/v2/management/transfers.rb
+++ app/api/v2/management/transfers.rb
@@ -4,77 +4,92 @@
 module API
   module V2
     module Management
-      class Transfers
+      class Transfers < Grape::API
 
         desc 'Creates new transfer.' do
           @settings[:scope] = :write_transfers
         end
         params do
-          requires :amount,
-                   type: BigDecimal,
-                   values: ->(v) { v.to_d.positive? },
-                   desc: 'Transfer amount.'
-          requires :currency,
+          requires :key,
+                   type: Integer,
+                   desc: 'Unique Transfer Key.'
+          requires :type,
                    type: String,
-                   values: -> { ::Currency.codes(bothcase: true) },
-                   desc: 'The currency code.'
+                   desc: 'Transfer Type.'
+          requires :desc,
+                   type: String,
+                   desc: 'Transfer Description.'
 
-          requires :from_type,
-                   type: String,
-                   values: Operation.TYPES,
-                   desc: 'From Account type.'
-          optional :from_kind,
-                   type: String,
-                   default: :main,
-                   desc: 'From Account kind.'
-          optional :from_uid,
-                   type: String,
-                   desc: 'Member UID for performing transfer from.'
+          requires(:operations, type: Array) do
+            requires :currency,
+                     type: String,
+                     values: -> { Currency.codes(bothcase: true) },
+                     desc: 'Operation currency.'
+            requires :amount,
+                     type: BigDecimal,
+                     values: ->(v) { v.to_d.positive? },
+                     desc: 'Operation amount.'
 
-          requires :to_type,
-                   type: String,
-                   values: Operation.TYPES,
-                   desc: 'To Account type.'
-          optional :to_kind,
-                   type: String,
-                   default: :main,
-                   desc: 'To Account kind.'
-          optional :to_uid,
-                   type: String,
-                   desc: 'Member UID for performing transfer to.'
+            requires :account_src, type: Hash do
+              requires :code,
+                       type: Integer,
+                       values: -> { ::Operations::Chart.codes },
+                       desc: 'Source Account code.'
+              given code: ->(code) { :member.in?(::Operations::Chart.find_chart(code)[:scope]) } do
+                requires :uid,
+                         type: String,
+                         desc: 'Source Account User ID (for accounts with member scope).'
+              end
+            end
+
+            requires :account_dst, type: Hash do
+              requires :code,
+                       type: Integer,
+                       values: -> { ::Operations::Chart.codes },
+                       desc: 'Destination Account code.'
+              given code: ->(code) { :member.in?(::Operations::Chart.find_chart(code)[:scope]) } do
+                requires :uid,
+                         type: String,
+                         desc: 'Destination Account User ID (for accounts with member scope).'
+              end
+            end
+          end
         end
         post '/transfers/new' do
-          currency = Currency.find(params.fetch(:currency))
-
-          from_account_entry = Operations::Chart.entry_for(params.slice(:type, :kind).merge(currency_type: currency.type))
-          to_account_entry = Operations::Chart.entry_for(params.slice(:type, :kind).merge(currency_type: currency.type))
-
-          from_member =
-            if :member.in?(from_account_entry[:scope])
-              # TODO: Move this validation to params block.
-              from_uid = params.fetch(:from_uid) do
-                raise Grape::Exceptions::Validation, params: :from_uid, message: 'must be present.'
-              end
-              Member.find_by!(uid: from_uid)
-            end
-          to_member =
-            if :member.in?(to_account_entry[:scope])
-              # TODO: Move this validation to params block.
-              to_uid = params.fetch(:to_uid) do
-                raise Grape::Exceptions::Validation, params: :to_uid, message: 'must be present.'
-              end
-              Member.find_by!(uid: to_uid)
-            end
-
-          options = {
-            currency_id: params[:currency],
-            amount: params[:amount],
-            from_code: from_account_entry[:code],
-            to_code: to_account_entry[:code],
-            from_member: from_member,
-            to_member: to_member
-          }.compact
-          OpenStruct(options)
+          declared_params = declared(params)
+          binding.pry
+          declared_params
+          # currency = Currency.find(params.fetch(:currency))
+          #
+          # from_account_entry = Operations::Chart.entry_for(params.slice(:type, :kind).merge(currency_type: currency.type))
+          # to_account_entry = Operations::Chart.entry_for(params.slice(:type, :kind).merge(currency_type: currency.type))
+          #
+          # from_member =
+          #   if :member.in?(from_account_entry[:scope])
+          #     # TODO: Move this validation to params block.
+          #     from_uid = params.fetch(:from_uid) do
+          #       raise Grape::Exceptions::Validation, params: :from_uid, message: 'must be present.'
+          #     end
+          #     Member.find_by!(uid: from_uid)
+          #   end
+          # to_member =
+          #   if :member.in?(to_account_entry[:scope])
+          #     # TODO: Move this validation to params block.
+          #     to_uid = params.fetch(:to_uid) do
+          #       raise Grape::Exceptions::Validation, params: :to_uid, message: 'must be present.'
+          #     end
+          #     Member.find_by!(uid: to_uid)
+          #   end
+          #
+          # options = {
+          #   currency_id: params[:currency],
+          #   amount: params[:amount],
+          #   from_code: from_account_entry[:code],
+          #   to_code: to_account_entry[:code],
+          #   from_member: from_member,
+          #   to_member: to_member
+          # }.compact
+          # OpenStruct(options)
         end
       end
     end
