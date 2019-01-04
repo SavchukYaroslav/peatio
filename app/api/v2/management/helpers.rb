cd42dcddcfd004eae0bb0f52d1d273ede217bda9
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2019-01-04 16:59:09 +0200

    Refactor attr -> attrs

--- app/api/v2/management/helpers.rb
+++ app/api/v2/management/helpers.rb
@@ -5,63 +5,63 @@ module API
   module V2
     module Management
       module Helpers
-        def create_operation!(attr)
-          if attr.fetch(:type).in?(Operation::MEMBER_TYPES) \
-            && attr[:uid].present?
-            create_member_operation!(attr)
+        def create_operation!(attrs)
+          if attrs.fetch(:type).in?(Operation::MEMBER_TYPES) \
+            && attrs[:uid].present?
+            create_member_operation!(attrs)
           else
-            create_platform_operation!(attr)
+            create_platform_operation!(attrs)
           end
         end
 
         private
 
-        def create_platform_operation!(attr)
-          currency = Currency.find(attr.fetch(:currency))
-          klass = attr.delete(:type)
+        def create_platform_operation!(attrs)
+          currency = Currency.find(attrs.fetch(:currency))
+          klass = attrs.delete(:type)
                       .yield_self { |type| "operations/#{type}" }
                       .camelize
                       .constantize
 
-          if attr[:credit].present?
-            klass.credit!(amount: attr.fetch(:credit),
+          if attrs[:credit].present?
+            klass.credit!(amount: attrs.fetch(:credit),
                           currency: currency,
-                          **attr.slice(:kind, :code))
-          elsif attr[:debit].present?
-            klass.debit!(amount: attr.fetch(:debit),
+                          **attrs.slice(:kind, :code))
+          elsif attrs[:debit].present?
+            klass.debit!(amount: attrs.fetch(:debit),
                          currency: currency,
-                         **attr.slice(:kind, :code))
+                         **attrs.slice(:kind, :code))
           end
         end
 
-        def create_member_operation!(attr)
-          member = Member.find_by!(uid: attr.fetch(:uid))
-          currency = Currency.find(attr.fetch(:currency))
-          klass = attr.delete(:type)
+        def create_member_operation!(attrs)
+          member = Member.find_by!(uid: attrs.fetch(:uid))
+          currency = Currency.find(attrs.fetch(:currency))
+          klass = attrs.delete(:type)
                       .yield_self { |type| "operations/#{type}" }
                       .camelize
                       .constantize
 
-          if attr[:credit].present?
-            amount = attr.fetch(:credit)
+          if attrs[:credit].present?
+            amount = attrs.fetch(:credit)
 
             op = klass.credit!(amount: amount,
                                member_id: member.id,
                                currency: currency,
-                               **attr.slice(:kind, :code))
+                               **attrs.slice(:kind, :code))
 
             credit_legacy_balance!(amount: amount,
                                    member: member,
                                    currency: currency,
                                    kind: op.chart.kind)
             op
-          elsif attr[:debit].present?
-            amount = attr.fetch(:debit)
+          elsif attrs[:debit].present?
+            amount = attrs.fetch(:debit)
 
             op = klass.debit!(amount: amount,
                               member_id: member.id,
                               currency: currency,
-                              **attr.slice(:kind, :code))
+                              **attrs.slice(:kind, :code))
 
             debit_legacy_balance!(amount: amount,
                                   member: member,
