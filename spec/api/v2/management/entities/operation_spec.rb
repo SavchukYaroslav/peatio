c63ca819e428f86b41d9ccd20ffb17c1e9a5b1c8
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2019-01-16 16:52:55 +0200

    Fix specs

--- spec/api/v2/management/entities/operation_spec.rb
+++ spec/api/v2/management/entities/operation_spec.rb
@@ -11,14 +11,13 @@ describe API::V2::Management::Entities::Operation do
       it { expect(subject.code).to eq record.code }
       it { expect(subject.currency).to eq record.currency_id }
       it { expect(subject.created_at).to eq record.created_at.iso8601 }
-      it { expect(subject.respond_to?(:uid)).to be_falsey }
 
       context 'credit' do
         it { expect(subject.credit).to eq record.credit }
         it { expect(subject.respond_to?(:debit)).to be_falsey }
       end
 
-      context 'credit' do
+      context 'debit' do
         let(:record) { create(:asset, :debit) }
         it { expect(subject.debit).to eq record.debit }
         it { expect(subject.respond_to?(:credit)).to be_falsey }
@@ -42,7 +41,7 @@ describe API::V2::Management::Entities::Operation do
         it { expect(subject.respond_to?(:debit)).to be_falsey }
       end
 
-      context 'credit' do
+      context 'debit' do
         let(:record) { create(:asset, :debit) }
         it { expect(subject.debit).to eq record.debit }
         it { expect(subject.respond_to?(:credit)).to be_falsey }
